496e9ac670957e4668ad51a256f0848b46394163
Photon Cat <lkstc112233@gmail.com>
2018-03-06 18:59:20 -0500

    Skeleton

---
+++ Example from tutorial/Example from tutorial/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Example from tutorial
+//
+//  Created by 煦 柯 on 2018/3/6.
+//  Copyright © 2018年 Must Studio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Example from tutorial/Example from tutorialTests/Example_from_tutorialTests.swift
@@ -0,0 +1,36 @@
+//
+//  Example_from_tutorialTests.swift
+//  Example from tutorialTests
+//
+//  Created by 煦 柯 on 2018/3/6.
+//  Copyright © 2018年 Must Studio. All rights reserved.
+//
+
+import XCTest
+@testable import Example_from_tutorial
+
+class Example_from_tutorialTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
